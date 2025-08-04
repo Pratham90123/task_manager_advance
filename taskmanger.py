64de2d900e18582e7efea0143b7c937cf00177f5
Pratham90123 <achauhanprathamz84@gmail.com>
2025-08-04 16:49:49 +0530

    to view tasks

--- taskmanger.py
+++ taskmanger.py
@@ -15,3 +15,19 @@ def add_task(title):
     tasks.append({"id": task_id, "title": title, "completed": False})
     save_tasks(tasks)
     print(f"Task added: {title}")
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    with open(TASKS_FILE, "r") as file:
+        return json.load(file)
+    
+
+def view_tasks():
+    tasks = load_tasks()
+    if not tasks:
+        print("No tasks found.")
+        return
+    for task in tasks:
+        status = "✅" if task["completed"] else "❌"
+        print(f"{task['id']}. {task['title']} - {status}")
