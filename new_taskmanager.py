6c6d80db29442c7843bfdcefb059d3994f1205a8
Pratham90123 <achauhanprathamz84@gmail.com>
2025-08-06 16:56:18 +0530

    Add Task feature

---
+++ new_taskmanager.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+
+TASKS_FILE="task.json"
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+def add_task(title):
+    tasks = load_tasks()
+    task_id = len(tasks) + 1
+    tasks.append({"id": task_id, "title": title, "completed": False})
+    save_tasks(tasks)
+    print(f"Task added: {title}")
+
+
+
+
+
+
+
+
+
+
+
